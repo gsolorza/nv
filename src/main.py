b893e986d3c6508f886358ffca7e3491705e1f60
Samy Rodriguez <samyrodriguez@MacBook-Air-de-Samy.local>
2022-12-09 22:11:25 -0300

    This is a test

--- src/main.py
+++ src/main.py
@@ -129,6 +129,7 @@ def form_update(form_id):
     query = schema.Query(column="id", value=form_id)
     form_data = crud.display_full_form(query, db())
     form = ChecklistFormSales()
+    print("Tu sabes")
     return redirect(url_for("home"))
 
 
